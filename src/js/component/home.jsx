f5b66040b5ad3df5c23c9fb9186144ceb1adef5c
carlosluis11 <crlosluis111@gmail.com>
2023-03-28 21:59:48 +0000

    Todo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+let initialValue = [];
 const Home = () => {
+	const [text, setText] = useState(initialValue)
+	const [textList, setTextList] = useState('')
+	
+	const handleTextList = (event) => {
+		setTextList(event.target.value)
+	}
+	const handleNewText = (event) => {
+		if (event.key === 'Enter') {
+			const newText = {
+				object: textList,
+			}
+			const newTexts = [...text, newText];
+			setText(newTexts)
+			setTextList("")
+		};
+	};
+	const hadledRemove = (index) => {
+		setText(text.filter((_, i) => index !== i))
+
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="list">
+			<h1 className="title display-4"><i>TODOS</i></h1>
+			<input type="text" className="input" placeholder="What needs to be done?" value={textList} onChange={(event) => handleTextList(event)} onKeyDown={(event) => handleNewText(event)} />
+			<ul>
+				{text.map((line, index) => {
+					return (
+							<h4 className="line-task d-flex justify-content-between" key={`${line.object}-${index}`}>
+								{line.object}
+								<button className="btn btn-danger d-flex justify-content-end me-3" onClick={(index) => hadledRemove(index)}><i className="fas fa-times align-self-center "></i></button>
+							</h4>
+					);
+				})}
+			</ul>
+			<div className="marker">
+				<hr />
+				<p className="text-item"> <i>{text.length} Items Left</i></p>
+			</div>
 		</div>
+
 	);
 };
 
